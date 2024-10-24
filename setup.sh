44939f0fd8cce25e3547d742cab4db6abb518889
Eli <ekworthi@mtu.edu>
2024-10-24 16:55:56 -0400

    setup script

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+temp_file=$(mktemp)
+
+
+curl -L -o "temp_file" "https://github.com/skpomerv/data1201/archive/refs/heads/main.zip"
+
+unzip "temp_file" -d ~/data1201/labs
+
+mv data1201-main lab9
